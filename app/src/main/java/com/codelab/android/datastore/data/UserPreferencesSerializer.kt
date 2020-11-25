a544ee9857d244079f13fb49aab35ebd91dde2d1
Milos Petrencak <m_petrencak@utb.cz>
2020-11-25 16:43:26 +0100

    Initial commit

---
+++ app/src/main/java/com/codelab/android/datastore/data/UserPreferencesSerializer.kt
@@ -0,0 +1,18 @@
+import androidx.datastore.CorruptionException
+import androidx.datastore.Serializer
+import com.codelab.android.datastore.UserPreferences
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object UserPreferencesSerializer : Serializer<UserPreferences> {
+    override fun readFrom(input: InputStream): UserPreferences {
+        try {
+            return UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override fun writeTo(t: UserPreferences, output: OutputStream) = t.writeTo(output)
+}
